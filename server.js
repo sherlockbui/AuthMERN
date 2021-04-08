d4954a60016781ba720a2ebe6fa006cd4c84e16e
sherlockbui <51515020+sherlockbui@users.noreply.github.com>
2021-04-08 21:50:16 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use("/api/auth", require("./routers/auth"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
